e8bcdb50639a450808a08e08713cb63d70d1089e
Allan Ortiz <aortez@gmail.com>
2018-10-12 23:11:05 -0700

    Runs game of life in text mode!

--- pkg/hello_rusty.d.ts
+++ pkg/hello_rusty.d.ts
@@ -1,3 +1,13 @@
 /* tslint:disable */
+export enum Cell {Dead,Alive,}
 export function greet(arg0: string): void;
 
+export class Universe {
+free(): void;
+static  new(): Universe;
+
+ render(): string;
+
+ tick(): void;
+
+}
--- www/index.js
+++ www/index.js
@@ -1,3 +1,16 @@
 import * as wasm from "hello-rusty";
+import { Universe } from "hello-rusty";
 
-wasm.greet("person");
+wasm.greet("Hello person.  Let's play the game of life.");
+
+const pre = document.getElementById("game-of-life-canvas");
+const universe = Universe.new();
+
+const renderLoop = () => {
+  pre.textContent = universe.render();
+  universe.tick();
+
+  requestAnimationFrame(renderLoop);
+};
+
+requestAnimationFrame(renderLoop);
